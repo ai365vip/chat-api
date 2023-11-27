f7682f07baf03fd80aa2d4f0244993aacd74e65e
luxl-1379 <726857632@qq.com>
2023-11-27 02:56:49 -0500

    解决合并冲突

--- controller/relay-openai.go
+++ controller/relay-openai.go
@@ -32,7 +32,6 @@ func openaiStreamHandler(c *gin.Context, resp *http.Response, relayMode int) (*O
 	stopChan := make(chan bool)
 	var wg sync.WaitGroup
 	go func() {
-		var streamItems []string
 		for scanner.Scan() {
 			data := scanner.Text()
 			if len(data) < 6 { // ignore blank line or wrong format
@@ -44,33 +43,28 @@ func openaiStreamHandler(c *gin.Context, resp *http.Response, relayMode int) (*O
 			dataChan <- data
 			data = data[6:]
 			if !strings.HasPrefix(data, "[DONE]") {
-				streamItems = append(streamItems, data)
-			}
-		}
-		streamResp := "[" + strings.Join(streamItems, ",") + "]"
-		switch relayMode {
-		case RelayModeChatCompletions:
-			var streamResponses []ChatCompletionsStreamResponseSimple
-			err := json.Unmarshal(common.StringToByteSlice(streamResp), &streamResponses)
-			if err != nil {
-				common.SysError("error unmarshalling stream response: " + err.Error())
-				return // just ignore the error
-			}
-			for _, streamResponse := range streamResponses {
-				for _, choice := range streamResponse.Choices {
-					responseTextBuilder.WriteString(choice.Delta.Content)
-				}
-			}
-		case RelayModeCompletions:
-			var streamResponses []CompletionsStreamResponse
-			err := json.Unmarshal(common.StringToByteSlice(streamResp), &streamResponses)
-			if err != nil {
-				common.SysError("error unmarshalling stream response: " + err.Error())
-				return // just ignore the error
-			}
-			for _, streamResponse := range streamResponses {
-				for _, choice := range streamResponse.Choices {
-					responseTextBuilder.WriteString(choice.Text)
+				switch relayMode {
+				case RelayModeChatCompletions:
+					var streamResponse ChatCompletionsStreamResponse
+					err := json.Unmarshal([]byte(data), &streamResponse)
+					if err != nil {
+						common.SysError("error unmarshalling stream response: " + err.Error())
+						continue // just ignore the error
+					}
+					for _, choice := range streamResponse.Choices {
+						responseText += choice.Delta.Content
+					}
+				case RelayModeCompletions:
+					var streamResponse CompletionsStreamResponse
+					err := json.Unmarshal([]byte(data), &streamResponse)
+					if err != nil {
+						common.SysError("error unmarshalling stream response: " + err.Error())
+						continue
+					}
+					for _, choice := range streamResponse.Choices {
+						responseText += choice.Text
+					}
+
 				}
 			}
 		}
@@ -96,7 +90,7 @@ func openaiStreamHandler(c *gin.Context, resp *http.Response, relayMode int) (*O
 	if err != nil {
 		return errorWrapper(err, "close_response_body_failed", http.StatusInternalServerError), ""
 	}
-	return nil, responseTextBuilder.String()
+	return nil, responseText
 }
 
 func openaiHandler(c *gin.Context, resp *http.Response, promptTokens int, model string) (*OpenAIErrorWithStatusCode, *Usage) {
