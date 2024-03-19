d81afd91b293250d058c85274b9189908ad164e4
luxlzz6 <726857632@qq.com>
2024-03-19 15:02:56 +0800

    chore: add more info for panic fix

--- middleware/recover.go
+++ middleware/recover.go
@@ -13,11 +13,15 @@ func RelayPanicRecover() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		defer func() {
 			if err := recover(); err != nil {
-				common.SysError(fmt.Sprintf("panic detected: %v", err))
-				common.SysError(fmt.Sprintf("stacktrace from panic: %s", string(debug.Stack())))
+				ctx := c.Request.Context()
+				common.Errorf(ctx, fmt.Sprintf("panic detected: %v", err))
+				common.Errorf(ctx, fmt.Sprintf("stacktrace from panic: %s", string(debug.Stack())))
+				common.Errorf(ctx, fmt.Sprintf("request: %s %s", c.Request.Method, c.Request.URL.Path))
+				body, _ := common.GetRequestBody(c)
+				common.Errorf(ctx, fmt.Sprintf("request body: %s", string(body)))
 				c.JSON(http.StatusInternalServerError, gin.H{
 					"error": gin.H{
-						"message": fmt.Sprintf("Panic detected, error: %v. Please submit a issue here: https://github.com/ai365vip/chat-api", err),
+						"message": fmt.Sprintf("Panic detected, error: %v. Please submit an issue with the related log here: https://github.com/ai365vip/chat-api", err),
 						"type":    "chat_api_panic",
 					},
 				})
