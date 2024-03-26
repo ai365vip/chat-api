814347fcb3d22065bca8e7ad8a01a68c4b3e898b
luxlzz6 <726857632@qq.com>
2024-03-26 19:51:14 +0800

    feat: add gemini-1.5-pro

--- relay/channel/gemini/constants.go
+++ relay/channel/gemini/constants.go
@@ -1,6 +1,6 @@
 package gemini
 
 var ModelList = []string{
-	"gemini-pro",
-	"gemini-pro-vision",
+	"gemini-pro", "gemini-1.0-pro-001", "gemini-1.5-pro",
+	"gemini-pro-vision", "gemini-1.0-pro-vision-001",
 }
