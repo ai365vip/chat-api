69413114517a3f6c66d907bd5d4b716c69995645
luxlzz6 <726857632@qq.com>
2024-05-22 15:56:45 +0800

    新增gemini-1.5

--- web-admin/src/pages/Channel/EditChannel.js
+++ web-admin/src/pages/Channel/EditChannel.js
@@ -115,7 +115,7 @@ const EditChannel = (props) => {
                     localModels = ['hunyuan'];
                     break;
                 case 24:
-                    localModels = ['gemini-pro','gemini-pro-vision','gemini-1.5-pro'];
+                    localModels = ['gemini-1.0-pro-latest','gemini-1.0-pro-001','gemini-1.5-pro-latest','gemini-1.5-flash-latest','gemini-ultra','gemini-1.0-pro-vision-latest','gemini-1.0-pro-vision-001'];
                     break;
                 case 2:
                     localModels = ['midjourney'];
