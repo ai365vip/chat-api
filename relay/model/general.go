690d6d742603e2ce95b835a708cef88203bb3972
luxlzz6 <726857632@qq.com>
2024-08-14 10:27:05 +0800

    Optimize response_format parameter

--- relay/model/general.go
+++ relay/model/general.go
@@ -1,38 +1,34 @@
 package model
 
-type ResponseFormat struct {
-	Type string `json:"type,omitempty"`
-}
-
 type GeneralOpenAIRequest struct {
-	Model            string          `json:"model,omitempty"`
-	Messages         []Message       `json:"messages,omitempty"`
-	Prompt           any             `json:"prompt,omitempty"`
-	Stream           bool            `json:"stream,omitempty"`
-	System           string          `json:"system,omitempty"`
-	MaxTokens        int             `json:"max_tokens,omitempty"`
-	Temperature      float64         `json:"temperature,omitempty"`
-	Stop             any             `json:"stop,omitempty"`
-	TopP             float64         `json:"top_p,omitempty"`
-	TopK             int             `json:"top_k,omitempty"`
-	N                int             `json:"n,omitempty"`
-	Input            any             `json:"input,omitempty"`
-	Instruction      string          `json:"instruction,omitempty"`
-	Size             string          `json:"size,omitempty"`
-	Functions        any             `json:"functions,omitempty"`
-	FrequencyPenalty float64         `json:"frequency_penalty,omitempty"`
-	PresencePenalty  float64         `json:"presence_penalty,omitempty"`
-	ResponseFormat   *ResponseFormat `json:"response_format,omitempty"`
-	Seed             float64         `json:"seed,omitempty"`
-	Tools            []Tool          `json:"tools,omitempty"`
-	ToolChoice       any             `json:"tool_choice,omitempty"`
-	FunctionCall     any             `json:"function_call,omitempty"`
-	User             string          `json:"user,omitempty"`
-	LogProbs         bool            `json:"logprobs,omitempty"`
-	TopLogProbs      int             `json:"top_logprobs,omitempty"`
-	EncodingFormat   string          `json:"encoding_format,omitempty"`
-	Dimensions       int             `json:"dimensions,omitempty"`
-	AnthropicVersion string          `json:"anthropic_version,omitempty"`
+	Model            string    `json:"model,omitempty"`
+	Messages         []Message `json:"messages,omitempty"`
+	Prompt           any       `json:"prompt,omitempty"`
+	Stream           bool      `json:"stream,omitempty"`
+	System           string    `json:"system,omitempty"`
+	MaxTokens        int       `json:"max_tokens,omitempty"`
+	Temperature      float64   `json:"temperature,omitempty"`
+	Stop             any       `json:"stop,omitempty"`
+	TopP             float64   `json:"top_p,omitempty"`
+	TopK             int       `json:"top_k,omitempty"`
+	N                int       `json:"n,omitempty"`
+	Input            any       `json:"input,omitempty"`
+	Instruction      string    `json:"instruction,omitempty"`
+	Size             string    `json:"size,omitempty"`
+	Functions        any       `json:"functions,omitempty"`
+	FrequencyPenalty float64   `json:"frequency_penalty,omitempty"`
+	PresencePenalty  float64   `json:"presence_penalty,omitempty"`
+	ResponseFormat   any       `json:"response_format,omitempty"`
+	Seed             float64   `json:"seed,omitempty"`
+	Tools            []Tool    `json:"tools,omitempty"`
+	ToolChoice       any       `json:"tool_choice,omitempty"`
+	FunctionCall     any       `json:"function_call,omitempty"`
+	User             string    `json:"user,omitempty"`
+	LogProbs         bool      `json:"logprobs,omitempty"`
+	TopLogProbs      int       `json:"top_logprobs,omitempty"`
+	EncodingFormat   string    `json:"encoding_format,omitempty"`
+	Dimensions       int       `json:"dimensions,omitempty"`
+	AnthropicVersion string    `json:"anthropic_version,omitempty"`
 }
 
 func (r GeneralOpenAIRequest) ParseInput() []string {
