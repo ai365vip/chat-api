44a6b21c05f37a266f4d9d1bb1b3f5d168c4feeb
luxlzz6 <726857632@qq.com>
2025-02-03 00:01:33 +0800

    feat: add support for o3-mini models in model ratio

--- relay/channel/openai/adaptor.go
+++ relay/channel/openai/adaptor.go
@@ -95,6 +95,12 @@ func (a *Adaptor) ConvertRequest(c *gin.Context, meta *util.RelayMeta, request *
 			request.Temperature = nil
 		}
 	}
+	if request.Model == "o1" || request.Model == "o1-2024-12-17" || strings.HasPrefix(request.Model, "o3") {
+		//修改第一个Message的内容，将system改为developer
+		if len(request.Messages) > 0 && request.Messages[0].Role == "system" {
+			request.Messages[0].Role = "developer"
+		}
+	}
 	return request, nil
 }
 
