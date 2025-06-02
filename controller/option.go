343efef5283c98d10083bc5c3c5a1ac3cbed8117
StageDog <aksanajw845@gmail.com>
2025-06-03 07:52:37 +0800

    docs: 强调 discord 需要的设置

--- controller/option.go
+++ controller/option.go
@@ -70,10 +70,10 @@ func UpdateOption(c *gin.Context) {
 	}
 	switch option.Key {
 	case "DiscordOAuthEnabled":
-		if option.Value == "true" && config.DiscordClientId == "" {
+		if option.Value == "true" && config.ServerAddress == "" && config.DiscordClientId == "" && config.DiscordClientSecret == "" {
 			c.JSON(http.StatusOK, gin.H{
 				"success": false,
-				"message": "无法启用 Discord OAuth，请先填入 Discord Client Id 以及 Discord Client Secret！",
+				"message": "无法启用 Discord OAuth，请先填入服务器地址，Discord Client Id 以及 Discord Client Secret！",
 			})
 			return
 		}
