ec0c00f8e169f829ccf8ab6b9fc9e5f4701c234e
luxlzz6 <726857632@qq.com>
2025-07-12 00:28:20 +0800

    add：xai grok4

--- relay/channel/xai/constants.go
+++ relay/channel/xai/constants.go
@@ -1,13 +1,6 @@
 package xai
 
 var ModelList = []string{
-	// grok-3
-	"grok-3-beta", "grok-3-mini-beta",
-	// grok-3 mini
-	"grok-3-fast-beta", "grok-3-mini-fast-beta",
-	// image model
-	"grok-2-image",
-	// legacy models
-	"grok-2", "grok-2-vision",
-	"grok-beta", "grok-vision-beta",
+	"grok-3-beta", "grok-3-mini-beta", "grok-3-fast-beta", "grok-3-mini-fast-beta",
+	"grok-4-0709", "grok-3-mini-fast", "grok-3-mini", "grok-3-fast", "grok-3",
 }
