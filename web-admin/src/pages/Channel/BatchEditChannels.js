3e9f280aa082df2fc3e3b2e5b7a605d0334418d4
luxlzz6 <726857632@qq.com>
2025-07-12 00:31:18 +0800

    add：gemini-2.5

--- web-admin/src/pages/Channel/BatchEditChannels.js
+++ web-admin/src/pages/Channel/BatchEditChannels.js
@@ -25,6 +25,8 @@ const DEFAULT_GEMINI_MODELS = [
 	'gemini-2.0-pro-exp', 'gemini-2.0-pro-exp-02-05',
 	'gemini-2.0-flash-exp', 'gemini-2.0-flash-thinking-exp-01-21', 
 	'gemini-2.5-pro-exp-03-25', 'gemini-2.5-flash-preview-04-17',
+    'gemini-2.5-pro-preview-05-06', 'gemini-2.5-pro', 'gemini-2.5-flash-preview-05-20',
+    'gemini-2.5-pro-preview-06-05', 'gemini-2.5-flash-lite-preview-06-17',
 ];
 const BatchEditChannels = (props) => {
     
